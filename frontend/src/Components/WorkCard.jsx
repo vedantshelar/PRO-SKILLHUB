2efca13311be35d492ca79cdc74f9d4c862add0a
vedant shelar. <135502684+vedantshelar@users.noreply.github.com>
2025-07-26 21:13:52 +0530

    Update WorkCard.jsx

--- frontend/src/Components/WorkCard.jsx
+++ frontend/src/Components/WorkCard.jsx
@@ -5,7 +5,7 @@ function WorkCard({work,removeWork,isOwner}) {
         <div className={styles.workInfoCard}>
             <p className={styles.companyName}>{work.name}</p>
             <p className={styles.position}>Description-{work.position}</p>
-            <p className={styles.experience}>Experience-{work.experience}</p>
+            <p className={styles.experience}>Experience-{work.experience} months</p>
             {isOwner && <i className="fa-solid fa-trash" style={{color: "#dc1e1e",cursor:"pointer"}} onClick={()=>{removeWork(work._id)}}></i> }
         </div>
     );
